ae4fc9817e6962bffcc56a106fa437dcd36abc3f
WillyLu326 <luzh0326@gmail.com>
2016-12-04 12:00:28 -0500

    spread operator

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 /**
  * Created by zhenglu on 12/2/16.
  */
-console.log('hello');
+var bankAccount = {
+    money: 2000,
+    deposit: function (value) {
+        this.money += value;
+    }
+};
+var myself = {
+    name: "Max",
+    bankAccount: bankAccount,
+    hobbies: ["Sports", "Cooking"]
+};
+myself.bankAccount.deposit(3000);
+console.log(myself);
+var variable = null;
+console.log(variable);
+// Rest & Spread
+var numbers = [1, 2, 3, 4, 5];
+console.log(Math.max.apply(null, numbers)); // using apply to pass parameter
+console.log(Math.max.apply(Math, numbers)); // ... is called spread operator
--- app.ts
+++ app.ts
@@ -1,4 +1,32 @@
 /**
  * Created by zhenglu on 12/2/16.
  */
-console.log('hello');
+let bankAccount: {money: number, deposit: (val: number) => void} = {
+    money: 2000,
+    deposit(value: number): void {
+        this.money += value;
+    }
+};
+
+type BankAccount = {money: number, deposit: (val: number) => void };
+
+let myself: {name: string, bankAccount: BankAccount, hobbies: string[] } = {
+    name: "Max",
+    bankAccount: bankAccount,
+    hobbies: ["Sports", "Cooking"]
+};
+
+myself.bankAccount.deposit(3000);
+
+console.log(myself);
+
+let variable: string = null;
+
+console.log(variable);
+
+
+// Rest & Spread
+
+const numbers = [1, 2, 3, 4, 5];
+console.log(Math.max.apply(null, numbers)); // using apply to pass parameter
+console.log(Math.max(...numbers)); // ... is called spread operator
