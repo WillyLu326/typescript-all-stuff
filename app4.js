17058a0942b447639cae8a0fb3c0e5095f4054e1
WillyLu326 <luzh0326@gmail.com>
2016-12-05 15:20:56 -0500

    Decorator

--- app4.js
+++ app4.js
@@ -33,3 +33,46 @@ var Visitor = (function () {
     ], Visitor);
     return Visitor;
 }());
+// Useful Decorator
+function print(constructor) {
+    constructor.prototype.print = function () {
+        console.log(this);
+    };
+}
+var Animal = (function () {
+    function Animal() {
+        this.name = 'animal';
+    }
+    Animal = __decorate([
+        logging(true),
+        print
+    ], Animal);
+    return Animal;
+}());
+var animal = new Animal();
+animal.print();
+// Method Decorator
+function editable(value) {
+    return function (target, propName, descriptor) {
+        console.log(target, propName);
+        descriptor.writable = value;
+    };
+}
+var Project2 = (function () {
+    function Project2(name) {
+        this.name = name;
+    }
+    Project2.prototype.printNumber = function (num) {
+        return num;
+    };
+    __decorate([
+        editable(false)
+    ], Project2.prototype, "printNumber", null);
+    return Project2;
+}());
+var project2 = new Project2('name');
+console.log(project2.printNumber(1000));
+project2.printNumber = function (num) {
+    return num * 2;
+};
+console.log(project2.printNumber(1000));
--- app4.ts
+++ app4.ts
@@ -20,6 +20,46 @@ function logging(value: boolean) {
 }
 
 @logging(true)
-class Visitor {
+class Visitor {}
 
+// Useful Decorator
+function print(constructor: Function) {
+    constructor.prototype.print = function() {
+        console.log(this);
+    }
 }
+
+@logging(true)
+@print
+class Animal {
+    name = 'animal';
+}
+const animal = new Animal();
+(<any>animal).print();
+
+// Method Decorator
+function editable(value: boolean) {
+    return function (target: any, propName: string, descriptor: PropertyDescriptor) {
+        console.log(target, propName);
+        descriptor.writable = value;
+    }
+}
+class Project2 {
+    name: string;
+    
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    @editable(false)
+    printNumber(num: number): number {
+        return num;
+    }
+}
+
+const project2 = new Project2('name');
+console.log(project2.printNumber(1000));
+project2.printNumber = function(num: number): number {
+    return num * 2;
+}
+console.log(project2.printNumber(1000));
